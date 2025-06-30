6b60ad4e0ec4f55bacdcbe4788efa1bd81a23ec6
konrel <pa.jon97@gmail.com>
2025-06-29 22:06:22 -0700

    Update index.js

--- ElementCapture/index.js
+++ ElementCapture/index.js
@@ -1,60 +1,35 @@
 "use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // src/index.ts
-var index_exports = {};
-__export(index_exports, {
-  onLoad: () => onLoad,
-  onUnload: () => onUnload
-});
-module.exports = __toCommonJS(index_exports);
-var import_patcher = require("@vendetta/patcher");
-var import_metro = require("@vendetta/metro");
-var unpatch;
-var onLoad = () => {
-  const Button = (0, import_metro.findByProps)("Button", "IconButton");
-  if (!(Button == null ? void 0 : Button.default)) {
-    console.warn("[ClickLogger] Button component not found");
-    return;
+var { after } = globalThis.vendetta.patcher;
+var { findByProps } = globalThis.vendetta.metro;
+var unpatches = [];
+exports.default = {
+  onLoad: () => {
+    const Button = findByProps("IconButton", "TextButton", "Button");
+    if (!Button) {
+      console.log("[ClickLogger] Button module not found");
+      return;
+    }
+    const patch = after("default", Button, ([props], ret) => {
+      if (!(props == null ? void 0 : props.onPress) || props.__clickLoggerPatched) return;
+      props.__clickLoggerPatched = true;
+      const original = props.onPress;
+      props.onPress = (...args) => {
+        try {
+          const name = props.accessibilityLabel || props.testID || "Unnamed Button";
+          console.log(`[ClickLogger] Button clicked: ${name}`);
+        } catch (e) {
+          console.warn("[ClickLogger] Error logging click:", e);
+        }
+        original == null ? void 0 : original(...args);
+      };
+    });
+    unpatches.push(patch);
+    console.log("[ClickLogger] Logging hook installed.");
+  },
+  onUnload: () => {
+    for (const un of unpatches) un();
+    console.log("[ClickLogger] Logging hook removed.");
   }
-  unpatch = (0, import_patcher.after)("default", Button, ([props], comp) => {
-    const orig = props.onPress;
-    props.onPress = (...args) => {
-      var _a;
-      try {
-        const label = (props == null ? void 0 : props.accessibilityLabel) || ((_a = props == null ? void 0 : props.children) == null ? void 0 : _a.toString()) || "Unnamed button";
-        const icon = (props == null ? void 0 : props.icon) || (props == null ? void 0 : props.source);
-        console.log(`[ClickLogger] Button clicked: ${label}`, { icon, props });
-      } catch (e) {
-        console.warn("[ClickLogger] Logging failed", e);
-      }
-      if (typeof orig === "function") orig(...args);
-    };
-    return comp;
-  });
-  console.log("[ClickLogger] Hooked into global Button onPress");
 };
-var onUnload = () => {
-  if (unpatch) unpatch();
-};
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
-  onLoad,
-  onUnload
-});
