8da0e2f4672abfebfcc54dc1fd704569a478612f
konrel <pa.jon97@gmail.com>
2025-06-29 23:31:44 -0700

    Update

--- HideCallButtons/index.js
+++ HideCallButtons/index.js
@@ -25,12 +25,13 @@ __export(index_exports, {
 module.exports = __toCommonJS(index_exports);
 var import_patcher = require("@vendetta/patcher");
 var import_assets = require("@vendetta/ui/assets");
+var import_toasts = require("@vendetta/ui/toasts");
 var import_metro = require("@vendetta/metro");
 var import_plugin = require("@vendetta/plugin");
 var patches = [];
 var index_default = {
   onLoad() {
-    console.log("\u{1F525} HideCallButtons plugin loaded");
+    (0, import_toasts.showToast)("\u{1F525} HideCallButtons plugin loaded", (0, import_assets.getAssetIDByName)("ic_message"));
     import_plugin.storage.upHideVoiceButton ??= true;
     import_plugin.storage.upHideVideoButton ??= true;
     import_plugin.storage.dmHideCallButton ??= false;
@@ -40,12 +41,9 @@ var index_default = {
     let voiceCallAsset = (0, import_assets.getAssetIDByName)("ic_audio") ?? (0, import_assets.getAssetIDByName)("PhoneCallIcon");
     const videoAsset = (0, import_assets.getAssetIDByName)("video");
     const callAsset = (0, import_assets.getAssetIDByName)("nav_header_connect");
-    console.log("\u{1F4E6} Asset IDs:", {
-      videoCallAsset,
-      voiceCallAsset,
-      videoAsset,
-      callAsset
-    });
+    const show = (msg) => (0, import_toasts.showToast)(`\u{1F527} ${msg}`, (0, import_assets.getAssetIDByName)("ic_settings"));
+    if (!videoCallAsset || !voiceCallAsset || !videoAsset || !callAsset)
+      (0, import_toasts.showToast)("\u26A0\uFE0F Asset ID missing", (0, import_assets.getAssetIDByName)("Small"));
     const UserProfileActions = (0, import_metro.findByName)("UserProfileActions", false);
     const SimplifiedUserProfileContactButtons = (0, import_metro.findByName)("SimplifiedUserProfileContactButtons", false);
     const PrivateChannelButtons = (0, import_metro.find)((x) => {
@@ -56,7 +54,7 @@ var index_default = {
     const VideoButton = (0, import_metro.findByProps)("VideoButton");
     patches.push((0, import_patcher.after)("default", UserProfileActions, (_, comp) => {
       var _a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k, _l;
-      console.log("\u{1F9E9} Patch triggered: UserProfileActions", comp);
+      show("Patch: UserProfileActions");
       if (!import_plugin.storage.upHideVideoButton && !import_plugin.storage.upHideVoiceButton) return;
       let buttons = ((_e = (_d = (_c = (_b = (_a = comp == null ? void 0 : comp.props) == null ? void 0 : _a.children) == null ? void 0 : _b.props) == null ? void 0 : _c.children[1]) == null ? void 0 : _d.props) == null ? void 0 : _e.children) ?? ((_h = (_g = (_f = comp == null ? void 0 : comp.props) == null ? void 0 : _f.children[1]) == null ? void 0 : _g.props) == null ? void 0 : _h.children);
       if ((_i = buttons == null ? void 0 : buttons.props) == null ? void 0 : _i.children) buttons = buttons.props.children;
@@ -78,7 +76,7 @@ var index_default = {
     }));
     patches.push((0, import_patcher.after)("default", SimplifiedUserProfileContactButtons, (_, comp) => {
       var _a;
-      console.log("\u{1F9E9} Patch triggered: SimplifiedUserProfileContactButtons", comp);
+      show("Patch: SimplifiedUserProfileContactButtons");
       const buttons = (_a = comp == null ? void 0 : comp.props) == null ? void 0 : _a.children;
       if (!buttons) return;
       if (import_plugin.storage.upHideVoiceButton) delete buttons[1];
@@ -86,14 +84,14 @@ var index_default = {
     }));
     patches.push((0, import_patcher.after)("default", VideoButton, (_, comp) => {
       var _a, _b, _c, _d, _e;
-      console.log("\u{1F9E9} Patch triggered: VideoButton", comp);
+      show("Patch: VideoButton");
       if (!import_plugin.storage.hideVCVideoButton) return;
       const vcButtons = (_e = (_d = (_c = (_b = (_a = comp == null ? void 0 : comp.props) == null ? void 0 : _a.children) == null ? void 0 : _b.props) == null ? void 0 : _c.children) == null ? void 0 : _d.props) == null ? void 0 : _e.children;
       if (vcButtons) delete vcButtons[0];
     }));
     patches.push((0, import_patcher.after)("type", PrivateChannelButtons, (_, comp) => {
       var _a, _b, _c, _d, _e, _f;
-      console.log("\u{1F9E9} Patch triggered: PrivateChannelButtons", comp);
+      show("Patch: PrivateChannelButtons");
       let buttons = (_a = comp == null ? void 0 : comp.props) == null ? void 0 : _a.children;
       if (!buttons) return;
       if (((_c = (_b = buttons[0]) == null ? void 0 : _b.props) == null ? void 0 : _c.source) === void 0)
@@ -108,7 +106,7 @@ var index_default = {
     }));
     patches.push((0, import_patcher.after)("ChannelButtons", ChannelButtons, (_, comp) => {
       var _a, _b, _c, _d;
-      console.log("\u{1F9E9} Patch triggered: ChannelButtons", comp);
+      show("Patch: ChannelButtons");
       const buttons = (_a = comp == null ? void 0 : comp.props) == null ? void 0 : _a.children;
       if (!buttons) return;
       for (const [i, btnWrapper] of Object.entries(buttons)) {
